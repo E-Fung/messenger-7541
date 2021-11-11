f0ce4e39d65b031591ccb661b262bdb9ae0654aa
Eric Fung <ehhfung@gmail.com>
2021-11-10 23:18:45 -0500

    reverted db.js

--- server/db/db.js
+++ server/db/db.js
@@ -1,8 +1,7 @@
 const Sequelize = require("sequelize");
 
-const db = new Sequelize('messenger', 'postgres', '1', {
-  host: 'localhost',
-  dialect: 'postgres',
+const db = new Sequelize(process.env.DATABASE_URL || "postgres://localhost:5432/messenger", {
+  logging: false
 });
 
 module.exports = db;
