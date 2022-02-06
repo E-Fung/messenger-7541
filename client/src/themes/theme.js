5b9184bf03032456e29e4ac73717049fd292bf61
Eric Fung <ehhfung@gmail.com>
2022-02-06 15:22:03 -0500

    implemented theme.typography for font styles

--- client/src/themes/theme.js
+++ client/src/themes/theme.js
@@ -3,11 +3,29 @@ import { createMuiTheme } from "@material-ui/core";
 export const theme = createMuiTheme({
   typography: {
     fontFamily: "Open Sans, sans-serif",
+    h2: {
+      fontSize: "1.86rem",
+      lineHeight: "2.86rem",
+      fontStyle: "normal",
+    },
+    body1: {
+      fontStyle: "normal",
+      lineHeight: "normal"
+    },
+    body2: {
+      fontSize: "0.86rem",
+      fontWeight: "600",
+      fontStyle: "normal",
+      lineHeight: "1.14rem"
+    },
     fontSize: 14,
     button: {
       textTransform: "none",
       letterSpacing: 0,
-      fontWeight: "bold"
+      fontWeight: "bold",
+      fontStyle: "normal",
+      lineHeight: "normal",
+      fontSize: "1rem",
     }
   },
   overrides: {
